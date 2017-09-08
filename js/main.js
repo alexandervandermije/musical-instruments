33673557ecb5f0e7b523837439dd79dd40b8c1c7
Alexander van der Mije <alexandervandermije@gmail.com>
2017-09-08 12:06:43 +0200

    score + descriptions

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ gameApp.controller('MainController', function MainController($scope, $http) {
 		{name: 'trumpet', sound: 'empty', img: 'trumpet.jpg'},
 		{name: 'cello', sound: 'empty', img: 'cello.png'},
 		{name: 'violin', sound: 'empty', img: 'violin.jpg'},
-		{name: 'bass guitar', sound: 'empty', img: 'bassguitar.png'},
+		{name: 'bass guitar', sound: 'empty', img: 'bassguitar.png', description:'De basgitaar is een snaarinstrument (doorgaans viersnarig) dat in vele vormen van moderne niet-klassieke muziek wordt gebruikt. De stemming is standaard ’E – ’A – D – G; zoals een contrabas en gelijk aan de laagste vier snaren van een gitaar, maar dan een octaaf lager. Meestal wordt een basgitaar uitgevoerd met frets, hoewel fretloze basgitaren ook bestaan.'},
 		{name: 'electric guitar', sound: 'empty', img: 'electricGuitar.png'}
 	]
 	
 	$scope.instrumentsCurrentRound = [];
 	$scope.amountTried = 0;
 	$scope.score = 0;
+	$scope.roundCorrect = false;
 
 	function generateRound(){
 	    var max = $scope.allInstruments.length;
@@ -26,22 +27,31 @@ gameApp.controller('MainController', function MainController($scope, $http) {
 	        else
 	         i--;
 	    }
-	    console.log($scope.instrumentsCurrentRound);
 	    $scope.correctInstrument = $scope.instrumentsCurrentRound[Math.floor(Math.random() * $scope.instrumentsCurrentRound.length)];
 	}
 	generateRound();
 
 	$scope.checkAnswer = function()
 	{
-		console.log(this.instrument.name);
 		if(this.instrument.name == $scope.correctInstrument.name)
 		{
-			console.log('correct');
+			if($scope.amountTried == 0 )
+			{
+				$scope.score += 3;
+			}
+			else if($scope.amountTried == 1)
+			{
+				$scope.score += 2;
+			}
+			else
+			{
+				$scope.score += 1;
+			}
+			$scope.roundCorrect = true;
 		}
 		else
 		{
-			$scope.amountTried +=1 ;
-			console.log($scope.amountTried);
+			$scope.amountTried += 1;
 		}
 
 	}
